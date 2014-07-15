1d2972ffbc8280b68c686f89f8bcb55a5b54ace3
chandrabhan pal <pal.chandrabhan@gmail.com>
2014-07-15 22:16:25 +0530

    acadmic by c

---
+++ Academic/AcademicRequest.aspx
@@ -0,0 +1,165 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="AcademicRequest.aspx.cs" Inherits="Academic_AcademicRequest" %>
+<%@ Register src="~/UserControl/jobs-top.ascx" tagname="header" tagprefix="uc1" %>
+<%@ Register src="~/UserControl/Footer.ascx" tagname="footer" tagprefix="uc2" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Edumate</title>
+    <link href="../render/stylesheetedumate.css" rel="stylesheet" type="text/css" />
+
+    <link rel="stylesheet" href="../render/jquery.popup.css" type="text/css">
+<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+<script type="text/javascript" src="../js/jquery.popup.js"></script>
+
+<script type="text/javascript">
+    $(function () {
+        $(".js__p_start, .js__p_another_start").simplePopup();
+    });
+  </script>
+
+
+</head>
+<body>
+      <form id="frmjob" runat="server">
+<section>
+<uc1:header ID="header1" runat="server" />
+
+<section class="middle-seacrh-sectn">
+<section class="news-sectn">
+    <div class="nav-03" style="margin-bottom:5px;float:left;">
+<ul>
+<li><a href="FindAcademic.aspx" title="Fin Job">Find Academic</a></li>
+<li><a href="AcademicRequest.aspx" title="Job Seeker">Academic Request</a></li>
+</ul></div>
+   </section>
+
+  <p class="sngle-line"></p>
+<div class="main-container">
+
+<section class="middle-sectn">
+
+<h5 class="hdng-03">Academic Request</h5>
+<p class="paras-001">
+<asp:Label ID="lblViewMsg" runat="server" Font-Bold="True" Font-Size="Large" ></asp:Label>
+<table id="tblAcdRequest" runat="server">
+ <tr>
+    <td>Type</td>
+     <td>Category</td>
+      <td>Sub Category</td>
+        
+    </tr>
+        <tr>
+            <td><asp:DropDownList ID="ddlType" CssClass="DropdwonLong" runat="server">
+            <asp:ListItem>--Select Type--</asp:ListItem>
+            <asp:ListItem>Test</asp:ListItem>
+            <asp:ListItem>Notes</asp:ListItem>
+            <asp:ListItem>Practicle</asp:ListItem>
+             <asp:ListItem>Any</asp:ListItem>
+            </asp:DropDownList>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator4" runat="server" ErrorMessage="*" ControlToValidate="ddlType" ValidationGroup="g1CreateF" InitialValue="--Select Type--"></asp:RequiredFieldValidator>
+            </td>
+
+            <td><asp:DropDownList ID="ddlCategory" CssClass="DropdwonLong" runat="server" AutoPostBack="true"
+                onselectedindexchanged="ddlCategory_SelectedIndexChanged">
+                </asp:DropDownList>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ErrorMessage="*" ControlToValidate="ddlCategory" ValidationGroup="g1CreateF" InitialValue="--Select Category--"></asp:RequiredFieldValidator>
+                </td>
+            
+               <td><asp:DropDownList ID="ddlSubCategory" CssClass="DropdwonLong" runat="server"></asp:DropDownList>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ErrorMessage="*" ControlToValidate="ddlSubCategory" ValidationGroup="g1CreateF" InitialValue="--Select Sub-Category--"></asp:RequiredFieldValidator>
+        </td>
+             </tr>
+         
+           <tr> <td colspan="3">Title :</td>
+             </tr>
+             <tr>
+             <td colspan="3"><asp:TextBox ID="txtTitle" CssClass="txtbox-fld" width="634px" runat="server"></asp:TextBox></td>
+             </tr>
+           <tr> <td colspan="3">Request :</td></tr>
+           <tr>
+           <td colspan="3"><asp:TextBox ID="txtDescription" style="height:50px;width:634px;" CssClass="txtbox-fld" TextMode="MultiLine" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td>
+             <asp:Button ID="btnSave" runat="server" CssClass="bnt-sbmt-01" Text="Submit" ValidationGroup="g1CreateF"
+                onclick="btnSave_Click" /> || <asp:Button ID="btnCancel" CssClass="bnt-sbmt-01" 
+                    runat="server" Text="Cancel" onclick="btnCancel_Click" />
+            </td>
+        </tr>
+    </table>
+
+
+    <%--<--------------------------PopUp--------------------------------->--%>
+  <div class="p_anch"> <a id="link" runat="server" href="#" class="js__p_start"><asp:Literal ID="litClickHere" runat="server"></asp:Literal></a></div>
+<div class="p_body js__p_body js__fadeout"></div>
+
+
+
+<div class="popup js__popup js__slide_top"> <a href="#" class="p_close js__p_close" title="Close"></a>
+<div class="p_content">Welcome to Edumate<br>
+<a href="../Default.aspx" target="_blank">Sign In</a> ||
+<a href="../Add Institute/EduType.aspx" target="_blank">Sign Up</a>
+</div>
+</div>
+
+         <%--<--------------------------PopUp--------------------------------->--%>
+</p>
+
+
+</section>
+<section>
+<article class="btm-sectn">
+</article>
+</section>
+
+
+<section class="container-rght-sectn">
+
+<aside class="Registration-Form-sectn">
+<h4 class="hdng-form">Registration Form</h4>
+<div class="left-sectn-blk-02">
+<p><span class="user-updates">User Name</span></p>
+<div class="img-contnt-sectn">
+<div class="img-01"><img src="images/btm-img.jpg" alt="user Name" title="User Name"></div>
+<div class="img-contnt">
+<p class="paras-01">Lorem ipsum <br>
+Lorem ipsum dolor siter amet,</p>
+
+</div>
+</div>
+
+
+</div>
+<div class="left-sectn-blk-02">
+<div class="lft-news">
+<p style="margin-bottom:0;"><span class="user-updates-01">News</span></p>
+
+<div class="img-contnt-sectn">
+<div class="blks"></div>
+<div class="img-contnt">
+<p class="news-hdng-rght">UPSC Announces<br>
+<span class="paras-01">Lorem ipsum dolor siter amet,</span></p>
+
+</div><p class="paras-002">Lorem ipsum Lorem ipsum dolor siter amet,</br>
+Lorem ipsum Lorem ipsum dolor siter amet,</br>
+Lorem ipsum Lorem ipsum dolor siter amet,</p>
+<p class="readmore">more...</p>
+</div>
+</div>
+</div>
+</aside>
+<aside class="advrtng-sectn">
+<div class="advrtng"></div>
+</aside>
+
+</section>
+</div>
+<footer>
+<uc2:footer ID="footer1" runat="server" />
+
+</footer>
+</section>
+      </form>
+</body>
+</html>
---
+++ Academic/FindAcademic.aspx.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class Academic_FindAcademic : System.Web.UI.Page
+{
+    string strcon = ConfigurationManager.ConnectionStrings["cn"].ConnectionString;
+    string sLiteral = "";
+    string strsave, strsave1, strsave2, strsave3, strsave4;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack == true)
+        {
+            Bind_Category();
+        }
+    }
+    protected void btnSearch_Click(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection(strcon);
+         SqlCommand cmd = new SqlCommand("Sp_Bind_Academicdata", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "Sp_Bind_Academicdata";
+       try
+           {
+           SqlParameter[] paramsToStore = new SqlParameter[2];
+ 
+           paramsToStore[0] = new SqlParameter("@Academic_Cat", SqlDbType.NVarChar);
+           paramsToStore[0].Size=400;
+           cmd.Parameters.Add(paramsToStore[0]).Value =  ddlCategory.SelectedItem.ToString();
+           paramsToStore[1] = new SqlParameter("@Academic_Sub_Cat", SqlDbType.NVarChar);
+           paramsToStore[1].Size=400;
+           cmd.Parameters.Add(paramsToStore[1]).Value = ddlSubCategory.SelectedItem.ToString();
+        }
+        catch (Exception excp)
+            {
+                lblMsg.ForeColor = System.Drawing.Color.Red;
+                lblMsg.Text = excp.Message;
+            }
+            con.Open();
+            cmd.Connection = con;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+            if (dt.Rows.Count>0)
+            {
+                sLiteral = " <link href='../render/stylesheetedumate.css' rel='stylesheet' type='text/css' />";
+
+                for (int iPackage = 0; iPackage < dt.Rows.Count; iPackage++)
+                {
+
+
+                    try
+                    {
+                        sLiteral = sLiteral + "<article class='artcless-middle'>";
+                        sLiteral = sLiteral + "<h5 class='hdng-03'>";
+                        sLiteral = sLiteral + "<a style='color:#3D76AD;' href='AcademicDescription.aspx?CDsce=" + dt.Rows[iPackage]["Academic_Id"].ToString() + "'>" + dt.Rows[iPackage]["Academic_Title"].ToString() + "</a>";
+                        sLiteral = sLiteral + "</h5>";
+
+
+                        sLiteral = sLiteral + "<p class='hdng-04-deepak'>";
+                        sLiteral = sLiteral + dt.Rows[iPackage]["Academic_Type"].ToString();
+                        sLiteral = sLiteral + "</p>";
+                        sLiteral = sLiteral + "<p class='artcless-img-02'>";
+
+                        String sServices = "<img border='0' src='../Advertisement/" + dt.Rows[iPackage]["collegeImage"].ToString() + "' class='img-size'/>";
+                        sLiteral = sLiteral + sServices;
+                        sLiteral = sLiteral + "</p>";
+
+                        sLiteral = sLiteral + "<div class='artcless-container'>";
+                     
+strsave = "<span style='color:#4B4B4B;'>" + dt.Rows[iPackage]["Academic_Desc"].ToString() + "<span>" + 
+    "<a style='color:#3D76AD;font-weight:bold;margin-left: 47px;   margin-top:5px;position:absolute;' href='AcademicDescription.aspx?CDsce=" + dt.Rows[iPackage]["Academic_Id"].ToString() + "'>" + "...View More" + "</a>";
+
+                        strsave2 = "<span style='color:#5086BB'>" + "Posted by :" + "</span>" + "<span style='margin-left:7px;'>" + dt.Rows[iPackage]["collegeName"].ToString() + "</span>" + "<span style='color:#5086BB;float:right;'>" + dt.Rows[iPackage]["created_Date"].ToString() + " days ago" + "</span>";
+                       
+                        sLiteral = sLiteral + "</div>";
+
+                        sLiteral = sLiteral + strsave + "</br>" + strsave1 + "</br>" + strsave2 + "</br>" + strsave3 + "</br>";
+
+                        sLiteral = sLiteral + "</article>";
+                    }
+                    catch
+                    {
+                     
+                    }
+
+                }
+
+
+                litName.Text = sLiteral;
+            }
+        else
+            {
+
+            }
+    }
+    public void Bind_Category()
+    {
+        SqlConnection con = new SqlConnection(strcon);
+        SqlCommand cmd = new SqlCommand("Sp_Academic_Category_View", con);
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.CommandText = "Sp_Academic_Category_View";
+       try
+       {
+       SqlParameter[] paramsToStore = new SqlParameter[2];
+ 
+       paramsToStore[0] = new SqlParameter("@ViewStatus", SqlDbType.NVarChar);
+       paramsToStore[0].Size=20;
+       cmd.Parameters.Add(paramsToStore[0]).Value = 0 ;
+       paramsToStore[1] = new SqlParameter("@DelStatus", SqlDbType.NVarChar);
+       paramsToStore[1].Size=20;
+       cmd.Parameters.Add(paramsToStore[1]).Value = 0 ;
+ 
+  
+       }
+
+        catch (Exception excp)
+        {
+            string strMsg = excp.Message;
+            Response.Write("<script LANGUAGE='JavaScript' >alert('" + strMsg + "')</script>");
+        }
+        con.Open();
+        cmd.Connection = con;
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        DataSet ds = new DataSet();
+        da.Fill(ds);
+        if (ds != null && ds.Tables.Count > 0)
+        {
+            ddlCategory.DataSource = ds;
+            ddlCategory.DataTextField = "Academic_Cat";
+            ddlCategory.DataValueField = "Academic_Cat";
+            ddlCategory.DataBind();
+            ddlCategory.Items.Insert(0, new ListItem("--Select Category--", "0"));
+            con.Close();
+
+        }
+        else
+        {
+            Response.Write("<script LANGUAGE='JavaScript' >alert('Please Try Again')</script>");
+        }
+
+    }
+    public void Bind_Sub_Category()
+    {
+        SqlConnection con = new SqlConnection(strcon);
+        SqlCommand cmd = new SqlCommand("Sp_Academic_SubCategory_View", con);
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.CommandText = "Sp_Academic_SubCategory_View";
+       try
+           {
+           SqlParameter[] paramsToStore = new SqlParameter[1];
+ 
+           paramsToStore[0] = new SqlParameter("@Academic_Cat", SqlDbType.NVarChar);
+           paramsToStore[0].Size=400;
+           cmd.Parameters.Add(paramsToStore[0]).Value = ddlCategory.SelectedItem.ToString();
+
+        }
+        catch (Exception excp)
+        {
+            string strMsg = excp.Message;
+            Response.Write("<script LANGUAGE='JavaScript' >alert('" + strMsg + "')</script>");
+
+        }
+        con.Open();
+        cmd.Connection = con;
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        DataSet ds = new DataSet();
+        da.Fill(ds);
+        if (ds != null)
+        {
+            ddlSubCategory.DataSource = ds;
+            ddlSubCategory.DataTextField = "Academic_Sub_Cat";
+            ddlSubCategory.DataValueField = "Academic_Sub_Cat";
+            ddlSubCategory.DataBind();
+            ddlSubCategory.Items.Insert(0, new ListItem("--Select Sub-Category--", "0"));
+            con.Close();
+
+
+        }
+        else
+        {
+            Response.Write("<script LANGUAGE='JavaScript' >alert('Please Try Again')</script>");
+       
+        }
+      
+    }
+
+    protected void ddlCategory_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        
+            Bind_Sub_Category();
+     }
+}
